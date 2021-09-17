55dc7e7a74cc13544365b47835aaa4c463389b4b
Moritz Wenk <MoritzWenk@web.de>
2021-09-17 15:03:02 +0200

    Removed no longer needed files

--- src/pvoutput.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "main.h"
-
-class pvoutput
-{
-public:
-    pvoutput();
-    bool send(uint16_t _year, uint8_t _month, uint8_t _day, uint8_t _hour, uint8_t _minute, float _energyToday, float _power, float _dc_u, float _temperature);
-};
